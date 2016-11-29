b2aa4284dc193a4f365567176206af1763aa8000
owen <owen@owendemacbook-pro.local>
2016-11-29 22:27:36 +0800

    declare a property:sum add a addition, and log the result

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic) int sum;
+
 @end
 
 @implementation ViewController
@@ -17,6 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int a = 5;
+    int b = 10;
+    self.sum = a + b;
+    NSLog(@"The result is: %d", self.sum);
 }
 
 
