8aac6400815badbaa35323004f0c5bc228094fb2
owen <owen@owendemacbook-pro.local>
2016-11-29 20:44:23 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by owen on 16/11/29.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by owen on 16/11/29.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
